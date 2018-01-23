f4fc3f4901128b8c95f602d1768cf2d58d12d3d8
Stefan Billiet <stefanbilliet@me.com>
2018-01-23 14:49:07 +0100

    add heroku configuration

--- server/db/configureMongoose.js
+++ server/db/configureMongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 module.exports = () => {
-  mongoose.connect('mongodb://admin:admin@localhost:27017/todoApp')
+  const connectionString = process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27017/todoApp';
+  mongoose.connect(connectionString);
   return mongoose;
 };
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const configureMongoose = require('./db/configureMongoose');
 
 configureMongoose();
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
 });
